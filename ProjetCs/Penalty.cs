9a27ff769db514512fcfaa3b53f7c2f2dfbfb31c
Enzobu <enzo.palermo@epsi.fr>
2024-04-22 17:06:26 +0200

    Create Penalty.cs

---
+++ ProjetCs/Penalty.cs
@@ -0,0 +1,67 @@
+namespace ProjetCs;
+
+public abstract class Penalty
+{
+    #region Attributes
+
+    private int code;
+    private int duration;
+    private double longitude;
+    private double latitude;
+
+    #endregion
+
+    #region Getter/Setter
+
+    public int Code
+    {
+        get => code;
+        private set => code = value;
+    }
+
+    public int Duration
+    {
+        get => duration;
+        private set
+        {
+            if (duration < 0)
+            {
+                throw new Exception("Valeurs inférieure à 0")
+            }
+            duration = value;
+        }
+    }
+
+    public double Longitude
+    {
+        get => longitude;
+        private set => longitude = value;
+    }
+
+    public double Latitude
+    {
+        get => latitude;
+        private set => latitude = value;
+    }
+
+    #endregion
+
+    #region Méthods
+
+    
+
+    #endregion
+
+    #region Constuctors
+
+    protected Penalty(int code, int duration, double longitude, double latitude)
+    {
+        Code = code;
+        Duration = duration;
+        Longitude = longitude;
+        Latitude = latitude;
+    }
+
+    #endregion
+
+}
