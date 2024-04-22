5712bb4e3de93c45bc57ee636182d8a750f09c3d
Yanis Capelle <capelleyanis@gmail.com>
2024-04-22 15:45:17 +0200

    class Employee

---
+++ ProjetCs/Employee.cs
@@ -0,0 +1,37 @@
+﻿namespace ProjetCs;
+
+public class Employee : User
+{
+    #region Attributes
+    
+    private string role;
+    
+    #endregion
+    
+    #region Getter/Setter
+
+    public string Role
+    {
+        get { return role; }
+        private set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new Exception("The role cannot be null or empty");
+            }
+
+            role = value;
+        }
+    }
+    
+    #endregion
+    
+    #region Constructor
+    
+    public Employee(int id, string lastName, string firstName, DateTime birthdate, string role) : base(id, lastName, firstName, birthdate)
+    {
+        Role = role;
+    }
+    
+    #endregion
+}
