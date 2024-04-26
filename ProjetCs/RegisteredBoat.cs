4dca81f5e3e57534bf4e115a371022754e1a5fab
Yanis Capelle <capelleyanis@gmail.com>
2024-04-26 15:29:27 +0200

    class RegisteredBoat
    ceci est une classe

---
+++ ProjetCs/RegisteredBoat.cs
@@ -0,0 +1,44 @@
+namespace ProjetCs;
+
+public class RegisteredBoat : Boat
+{
+    #region Attributes
+    
+    private bool inRace;
+    private int raceTime;
+    private int realTime;
+    
+    #endregion
+    
+    #region Getter/Setter
+    public bool InRace
+    {
+        get => inRace;
+        set => inRace = value;
+    }
+
+    public int RaceTime
+    {
+        get => raceTime;
+        set => raceTime = value;
+    }
+
+    public int RealTime
+    {
+        //set raceTime + Penalty.duration
+    }
+    
+    #endregion
+
+    #region Constructors
+
+    public RegisteredBoat(int id, bool inRace, int raceTime, int realTime) : base(id)
+    {
+        InRace = inRace;
+        RaceTime = raceTime;
+        //RealTime = this.GetRealTime();
+    }
+
+    #endregion
+    
+}
