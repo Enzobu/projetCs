5b2bd783acc108c3b0283df85b3e028e9e3e1da1
Yanis Capelle <capelleyanis@gmail.com>
2024-05-16 14:37:44 +0200

    initiation entity framework

---
+++ ProjetCs/Models/Service2bateauContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetCs.Models;
+
+public partial class Service2bateauContext : DbContext
+{
+    public Service2bateauContext()
+    {
+    }
+
+    public Service2bateauContext(DbContextOptions<Service2bateauContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseMySQL("Server=localhost;Database=service2bateau;Uid=root");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
